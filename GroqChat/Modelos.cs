1f406e048ce03be9c67b1e6fd40e93b0cfaaea69
RigorVzla <41328887+rigorvzla@users.noreply.github.com>
2024-09-16 17:55:52 -0400

    Add files via upload

---
+++ GroqChat/Modelos.cs
@@ -0,0 +1,13 @@
+﻿namespace GroqSharp
+{
+    internal class Modelos
+    {
+        public enum GroqModel
+        {
+            Mixtral8x7b32768,
+            Llama38b8192,
+            Llama370b8192,
+            Gemma7bIt
+        }
+    }
+}
